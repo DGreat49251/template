6e074eea2a207b01431b85b8fc15afd974f764d0
Dipak Ghosh <70680058+DGreat49251@users.noreply.github.com>
2021-07-28 12:15:42 +0530

    Delete components directory

--- components/button/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './Button.css';
-
-export function Button(props) {
-  return (
-      <a className='btn' href={props.url}>{props.text}</a>
-  );
-}
-export default Button;
